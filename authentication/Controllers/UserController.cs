fc49c74128a726fc8ab599d3e8c654f976520578
Rushi Rajwade <rushi.rajwade@promptsoftech.in>
2023-09-26 19:00:59 +0530

    updated files

--- authentication/Controllers/UserController.cs
+++ authentication/Controllers/UserController.cs
@@ -1,9 +1,17 @@
 ﻿using authentication.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using Vonage;
+using Vonage.Common;
+using Vonage.Request;
 
 namespace authentication.Controllers
 {
@@ -64,11 +72,12 @@ namespace authentication.Controllers
         public ActionResult MatchOtp(string no)
         {
             LoginModel loginModel = new LoginModel();
-
-            UserModel user = new UserModel();
-            if(user.GetOtp(no)!=null )
+            
+            DataAccess user = new DataAccess();
+            Session["key"] = user.GetOtp(no);
+            if (user.GetOtp(no)!=null )
             {
-                bool otp = user.verifyOTP("465704",no);
+                bool otp = user.verifyOTP("26268", no);
                 if(otp)
                 {
                     return Json(new{message="login" });
@@ -86,20 +95,23 @@ namespace authentication.Controllers
 
         }
 
-
+        public ActionResult generateotp()
+        {
+            return View();
+        }
         //  public string connectionString = "data source=(LocalDb)\\MSSQLLocalDB;initial catalog=auth;persist security info=True;";
         [HttpPost]
-        public ActionResult generateotp(string mobilenumber)
+        public ActionResult generateotp(string MobileNo)
         {
             
             try
             {
-                UserModel model = new UserModel();
-                bool existMobile = model.MobileNumberExists(mobilenumber);
+                DataAccess model = new DataAccess();
+                bool existMobile = model.MobileNumberExists(MobileNo);
                 if (existMobile) {
                     string otp = model.GenerateRandomOTP(6);
                     DateTime expiretime= DateTime.Now.AddMinutes(5);
-                    int userid=model.GetUserIdByMobileNumber(mobilenumber);
+                    int userid=model.GetUserIdByMobileNumber(MobileNo);
 
                     if(userid > 0)
                     {
@@ -128,6 +140,141 @@ namespace authentication.Controllers
             }
 
         }
+   
+
+// Replace these values with your Vonage credentials
+string apiKey = "e3cb936f";
+    string apiSecret = "CYknodsz77JxHXx8";
+    string vonagePhoneNumber = "918237382320";
+
         
+        public ActionResult SendOtpViaSms()
+    {
+            DataAccess userModel = new DataAccess();  
+        var otp = userModel.GenerateRandomOTP(6);
+        string userPhoneNumber = "918401095050"; 
+
+
+            //var credentials = Credentials.FromApiKeyAndSecret(apiKey, apiSecret);
+            //var client = new VonageClient(credentials);
+            var credentials = Credentials.FromApiKeyAndSecret(
+    "e3cb936f",
+    "CYknodsz77JxHXx8"
+    );
+
+            var VonageClient = new VonageClient(credentials);
+
+            var response = VonageClient.SmsClient.SendAnSms(new Vonage.Messaging.SendSmsRequest()
+            {
+                To = "918237382320",
+                From = "Vonage APIs",
+                Text = $"A text message sent using the Vonage SMS API:{otp}"
+            });
+            //    var response = client.SmsClient.SendAnSms(new Vonage.Messaging.SendSmsRequest
+            //{
+            //    To = userPhoneNumber,
+            //    From = vonagePhoneNumber,
+            //    Text = $"Your OTP is: {otp}"
+            //});
+
+            // Store the OTP on the server temporarily, associated with the user's session or request context
+            Session["UserOTP"] = otp;
+
+        return View();
     }
+
+        //public ActionResult verifyotp(string userotp)
+        //   {
+        //       int? storedotp = Session["otp"] as int?;
+        //       if (storedotp == 0)
+        //       {
+        //           return Json(new {success=false,Message="otp not found"});
+        //       }
+        //       if (storedotp.Value.ToString() == userotp)
+        //       {
+        //           return RedirectToAction("Index");
+        //           Session["otp"] = null;
+        //           return Json(new { success = true, Message = "otp verified" });
+        //       }
+        //       return Json (new
+        //       {
+        //           success=false,
+        //           Message="otp not verified"
+        //       });
+        //   }
+        public ActionResult verify()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult verify(string userotp)
+        {
+            if (userotp == Session["otp"].ToString())
+            {
+                return RedirectToAction("About","Home");
+            }
+            else { return Json(0);
+            }
+        }
+
+        public ActionResult sendotp()
+        {
+            return View();
+        }
+        [HttpPost]
+    public ActionResult sendotp(string Mobileno)
+        {
+            DataAccess dataAccess = new DataAccess();
+            bool ismobile=dataAccess.MobileNumberExists(Mobileno);
+          if (!ismobile)
+            {
+                return Json(new { success = false, message = "Invalid phone number format." });
+            }
+            Random rand = new Random();
+            int value=rand.Next(100001,999999);
+            //string address = "+918237382320";
+            string otp = $"Your otp is:{value}";
+            
+            using (var wb=new WebClient())
+            { 
+                byte[] response = wb.UploadValues("https://api.textlocal.in/send/", new NameValueCollection()
+                {
+                    {"apikey","NTA2ODRhNGU0ZDUyNmY0MzQ2NmQ1YTczNjQ1YTM2N2E=" },
+                    {"numbers",Mobileno },
+                    {"sender","TXTLCL" }
+                });
+                string result=System.Text.Encoding.UTF8.GetString(response);
+                Session["otp"] = value;
+            }
+            return RedirectToAction("verify","User");
+            return Json(new { success = true, message = "OTP sent successfully." });
+           // return View();
+        }
+    //private string accountsid = "AC12b92edc7fc54296db745d34dc4288df";
+    //string accounttoken = "e69dd6eafe6a5a81bf973f88b8ffbc87";
+    //string phonenumber = "+918237382320";
+    //public ActionResult SendOtpViaSms()
+    //{
+    //    UserModel userModel = new UserModel();  
+
+    //    var otp = userModel.GenerateRandomOTP(6); // Implement this method to generate a random OTP
+    //    string userPhoneNumber ="+918830761630"; // Get the user's phone number from your database
+
+    //    TwilioClient.Init(accountsid, accounttoken);
+
+    //    var message = MessageResource.Create(
+    //        body: $"Your OTP is: {otp}",
+    //        from: new PhoneNumber(phonenumber),
+    //        to: new PhoneNumber(userPhoneNumber)
+    //    );
+
+    //    // Store the OTP on the server temporarily, associated with the user's session or request context
+    //    Session["UserOTP"] = otp;
+
+    //    return View();
+    //}
+}
+    
+
+    
 }
--- authentication/Models/LoginModel.cs
+++ authentication/Models/LoginModel.cs
@@ -7,10 +7,25 @@ namespace authentication.Models
 {
     public class LoginModel
     {
-      //  public int id { get; set; }
+        public int userId { get; set; }
+       public string firstname{ get; set; }
+       public string lastname{ get; set; }
         public string userEmail { get; set; }
         public string userPassword { get; set; }
+     
+        public string MobileNo { get; set; }
+        public string Gender { get; set; }
+        public DateTime Dob { get; set; }
+        public string Roles { get; set; }
     }
-    
+
+    public class LoginUser
+    {
+        public int LoginUserId { get; set; }
+        public string LoginOtp { get; set; }
+
+        public DateTime ExpirationLoginTime { get; set; }
+        public int userIdByLoginUser { get; set; }
+    }
 
 }
